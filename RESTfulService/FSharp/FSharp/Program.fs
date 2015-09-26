fa694d604a78f27ab40048d9d35e6d4724da7186
Dima <dmitry.korolev@gmail.com>
2015-09-26 13:30:41 -0700

    RESTful server+test placeholder.

--- RESTfulService/FSharp/FSharp/Program.fs
+++ RESTfulService/FSharp/FSharp/Program.fs
@@ -1,6 +1,32 @@
-﻿open Suave
+﻿open System
+open Suave
+open HttpClient
+open FsUnit
+open NUnit.Framework
+
+let port = 8083
+
+let service_routes = Http.Successful.OK "OK!\n"
+
+let spawn_server routes = 
+    let cancel = new Threading.CancellationTokenSource()
+    let config = { Web.defaultConfig with cancellationToken = cancel.Token }
+    let run_async async_server = ignore <| Async.StartAsTask(async_server, cancellationToken = cancel.Token)
+    Web.startWebServerAsync config routes |> snd |> run_async
+    cancel
+
+[<TestFixture>]
+type UnitTest() = 
+    [<Test>]
+    member this.ReturnsOK() =
+        let server = spawn_server service_routes
+        "OK!\n" |> should equal <| (createRequest Get (sprintf "http://localhost:%d/" port) |> getResponseBody)
+        server.Dispose() |> ignore
 
 [<EntryPoint>]
 let main argv =
-    Web.defaultConfig |> Web.startWebServer <| Http.Successful.OK "OK!\n"
+    let server = spawn_server service_routes
+    printfn "Server running on port %d. Press enter to stop it." port
+    System.Console.ReadLine() |> ignore
+    server.Dispose() |> ignore
     0
