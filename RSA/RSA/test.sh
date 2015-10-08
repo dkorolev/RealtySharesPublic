aa8ab4206d2e0abe42c490d1274d6b07607d420e
Dima <dmitry.korolev@gmail.com>
2015-10-07 21:00:53 -0700

    Made it work on Mac too.

--- RSA/RSA/test.sh
+++ RSA/RSA/test.sh
@@ -4,8 +4,8 @@ cat <<EOF >input.txt
 This is a simple input file to encrypt.
 EOF
 
-./Program.exe genkey key_public.xml key_private.xml
-./Program.exe encrypt key_public.xml input.txt encrypted.txt
-./Program.exe decrypt key_private.xml encrypted.txt output.txt
+mono Program.exe genkey key_public.xml key_private.xml
+mono Program.exe encrypt key_public.xml input.txt encrypted.txt
+mono Program.exe decrypt key_private.xml encrypted.txt output.txt
 
 diff input.txt output.txt && echo OK || echo Fail.
